a8fdff010aea07d8930492484ec7763b0b88e51d
dhordyko <dhordyko@gmail.com>
2026-01-25 23:18:14 -0600

    skill block set up

--- plugins/blocks-portfolio/build/block-skill/index.asset.php
+++ plugins/blocks-portfolio/build/block-skill/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '9169e44f908c82e99ae8');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'd38bf9664e117638f5d5');
